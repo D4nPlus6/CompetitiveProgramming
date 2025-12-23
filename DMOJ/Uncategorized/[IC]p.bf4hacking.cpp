ea364e767c8cdc9bbbdd1dc6744d9d02507e8511
dave <d4nplus6@gmail.com>
2025-12-23 14:15:46 -0500

    will work on/learn string algos later

--- DMOJ/Uncategorized/[IC]p.bf4hacking.cpp
+++ DMOJ/Uncategorized/[IC]p.bf4hacking.cpp
@@ -3,21 +3,10 @@
 using namespace std;
 
 int main() {
-    for (int i = 0; i < 500000; i++) {
-        cout << "ab";
-    }
+    for (int i = 0; i < 500000; i++) {cout << "ab";}
     cout << '\n';
-
-    for (int i = 0; i < 250000; i++) {
-        cout << "ab";
-    }
+    for (int i = 0; i < 250000; i++) {cout << "ab";}
     cout << 'a' << '\n';
-
+    
     return 0;
 }
-
-
-
-// # String Finding - Hacking
-// print("a" * 1000000)
-// print("a" * 499999 + "b")
