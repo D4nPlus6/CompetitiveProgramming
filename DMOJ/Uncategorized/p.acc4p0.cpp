584d87fdb43bd82c6042da30be4081c7140c3b36
dave <d4nplus6@gmail.com>
2025-12-23 21:33:24 -0500

    meme problems

---
+++ DMOJ/Uncategorized/p.acc4p0.cpp
@@ -0,0 +1,11 @@
+// Another Contest 4 Problem 0 - The April Fools Warmup Problem
+#include <bits/stdc++.h>
+using namespace std;
+
+string f() {
+    return "All Kill";
+}
+
+int main() {
+    return 0;
+}
