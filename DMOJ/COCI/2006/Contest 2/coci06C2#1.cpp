745fe78f963ffb6f84dec08becc3743e523f7f54
dave <d4nplus6@gmail.com>
2025-12-24 22:14:22 -0500

    easy math 3p

---
+++ DMOJ/COCI/2006/Contest 2/coci06C2#1.cpp
@@ -0,0 +1,11 @@
+// COCI '06 Contest 2 #1 R2
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int16_t S,R;
+    cin >> R >> S;
+    cout << (S*2)-R << "\n";
+    return 0;
+}
