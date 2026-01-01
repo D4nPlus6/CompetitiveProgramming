cec90ccae418eaab8a3f3e59d4f1b40fefd05317
dave <d4nplus6@gmail.com>
2026-01-01 13:53:44 -0500

    shoutout to my goats Bruce, do_ur_homwork, Mark/Marks/Max/Marx (never figured out his name) Bruce Lee, Kim Jihoon, Yun Tong-Ju, Kokma, Binah, Ryan Guan/Ryan Guo (never figured out his name), and Bernie Liu

--- DMOJ/Bulgarian OI/2009/p5/build.py
+++ DMOJ/Bulgarian OI/2009/p5/build.py
@@ -1,1 +1,1 @@
-with open("/home/$USER/Work/CS/Personal/CompetitiveProgramming/DMOJ/Bulgarian OI/2009/p5/troll.txt","r") as f: print(f.readlines())
+with open("Bulgarian OI/2009/p5/prediction.txt","r") as f: print([int(i) for i in f.readlines()])
